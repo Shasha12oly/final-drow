d735071dc1229aa001697faf709b80a5f5b1549c
Shasha12oly <147236264+Shasha12oly@users.noreply.github.com>
2026-02-04 19:48:38 +0530

    Delete js directory

--- js/auth-guard.js
+++
@@ -1,5 +0,0 @@
-// Authentication Guard System - DISABLED
-// This file is disabled to prevent redirect loops
-// Dashboard has its own simple authentication check
-
-console.log('Auth guard disabled - using dashboard authentication only');
